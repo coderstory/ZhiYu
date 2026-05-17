53457468e05223d2df153a8558c76c69f3916456
coderstory <coderstory@163.com>
2026-05-18 01:36:48 +0800

    auto: WIP 2026-05-18 01:36

--- app/src/main/java/com/zhiyu/app/di/ViewModelModule.kt
+++ app/src/main/java/com/zhiyu/app/di/ViewModelModule.kt
@@ -5,7 +5,7 @@ import com.zhiyu.app.ui.screens.knowledge.ArticleDetailViewModel
 import com.zhiyu.app.ui.screens.knowledge.EditorViewModel
 import com.zhiyu.app.ui.screens.knowledge.KnowledgeViewModel
 import com.zhiyu.app.ui.screens.profile.ProfileViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
